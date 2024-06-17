fb00b67c1e3bd5a868014f9199281cab46634ef0
jeoungsung12 <jeoungsung12@naver.com>
2024-06-17 15:48:37 +0900

    initial push

---
+++ RightNow/Feat/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RightNow
+//
+//  Created by 정성윤 on 2024/06/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+    }
+}
+
