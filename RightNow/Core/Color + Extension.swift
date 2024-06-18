d7f51412012a5003bc30b089796262efa44cdc73
jeoungsung12 <jeoungsung12@naver.com>
2024-06-18 15:56:56 +0900

    Flask Test

---
+++ RightNow/Core/Color + Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Color + Extension.swift
+//  RightNow
+//
+//  Created by 정성윤 on 2024/06/18.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let pointColor : UIColor = {
+        return UIColor(named: "pointColor") ?? .white
+    }()
+}
